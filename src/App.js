38789b3dbc2213f2a0ad7b8a0dc9df9d8b5c7aff
Mateus Queiroz <mateus.queiroz@pitang.com>
2020-01-25 18:29:44 -0300

    (feature): add styled-components and global styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
+import GlobalStyles from './styles/global';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes />
+        <GlobalStyles />
       </Router>
     </div>
   );
