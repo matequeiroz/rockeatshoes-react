35e740ed39077215f0b122f3c844291487c8131b
Mateus Queiroz <mateus.queiroz@pitang.com>
2020-01-25 19:00:36 -0300

    (feature): styling of header of application

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 import GlobalStyles from './styles/global';
+import Header from './components/Header/index';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes />
         <GlobalStyles />
+        <Header />
       </Router>
     </div>
   );
