5bba1ffb907e422bce486c3ff1d6434fec76f9d0
Mateus Queiroz <mateusqueirozdev@gmail.com>
2020-01-26 11:21:10 -0300

    (feature): add product in cart

--- src/store/reducers/cart/index.js
+++ src/store/reducers/cart/index.js
@@ -3,7 +3,13 @@ const INITIAL_STATE = [];
 export default function cart(state = INITIAL_STATE, action) {
   switch (action.type) {
     case '@cart/ADD':
-      return [...state, action.payload];
+      return [
+        ...state,
+        {
+          ...action.payload,
+          amount: 1,
+        },
+      ];
     default:
       return state;
   }
