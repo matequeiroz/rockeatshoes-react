400ec0c1129bf0e8fd722b1d4e49ad8e0425b636
Mateus Queiroz <mateusqueirozdev@gmail.com>
2020-01-25 22:59:09 -0300

    (feature): add intregation react with redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Routes from './routes';
 import GlobalStyles from './styles/global';
+import store from './store';
 import Header from './components/Header/index';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <Routes />
-        <GlobalStyles />
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Header />
+          <Routes />
+          <GlobalStyles />
+        </Router>
+      </Provider>
     </div>
   );
 }
