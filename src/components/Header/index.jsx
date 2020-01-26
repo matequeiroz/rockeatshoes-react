378736624af3bac2c9d5558546a2ab6f1575d2e0
Mateus Queiroz <mateusqueirozdev@gmail.com>
2020-01-25 23:43:26 -0300

    (feature): add shoes in reducer of cart and show quantity of products adding in cart

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
+import { connect } from 'react-redux';
 
 import { Container, Cart } from '../../styles/Header/style';
 import logo from '../../assets/images/logo.svg';
 
-export default function Header() {
+function Header({ cartSize }) {
   return (
     <Container>
       <Link to="/">
@@ -15,10 +16,18 @@ export default function Header() {
       <Cart to="/cart">
         <div>
           <strong>Seu carrinho</strong>
-          <span>Carrinho vazio</span>
+          {cartSize > 0 ? (
+            <span>{cartSize} itens</span>
+          ) : (
+            <span>Carrinho vazio</span>
+          )}
         </div>
         <MdShoppingBasket size={36} color="#fff" />
       </Cart>
     </Container>
   );
 }
+
+export default connect(state => ({
+  cartSize: state.cart.length,
+}))(Header);
