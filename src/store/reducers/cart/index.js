ff7f9e1847076e0b5e5694c046d217b6f791506a
Mateus Queiroz <mateusqueirozdev@gmail.com>
2020-01-26 11:37:36 -0300

    (feature): add lib immer for immutable state and verify if product exists in cart for increase the quantity

--- src/store/reducers/cart/index.js
+++ src/store/reducers/cart/index.js
@@ -1,15 +1,22 @@
+import produce from 'immer';
 const INITIAL_STATE = [];
 
 export default function cart(state = INITIAL_STATE, action) {
   switch (action.type) {
     case '@cart/ADD':
-      return [
-        ...state,
-        {
-          ...action.payload,
-          amount: 1,
-        },
-      ];
+      const nextState = produce(state, draftState => {
+        const productIndex = draftState.findIndex(
+          item => item.id === action.payload.id
+        );
+
+        if (productIndex >= 0) {
+          draftState[productIndex].amount += 1;
+        } else {
+          draftState.push({ ...action.payload, amount: 1 });
+        }
+      });
+
+      return nextState;
     default:
       return state;
   }
